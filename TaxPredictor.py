bbcdd777ce3de4c5e73392a5c27a26bc61b527b1
Kaiserin_der_Welt <42034666+KaiserinDerWelt@users.noreply.github.com>
2019-01-28 23:08:05 -0600

    Create TaxPredictor.py
    Conditions on python

---
+++ TaxPredictor.py
@@ -0,0 +1,12 @@
+# Another Contribution of Kaiserin_der_Welt
+
+__author__ = "Kaiserin Der Welt"
+
+print('Welcome to the Tax Calculator.')
+print('Please answer the next questions')
+Salary=input('What is your brutto salary before taxes in Germany?')
+if Salary>=17001:
+    print'Your tax rate is -30% to 35%'
+else:
+        print'You are a small contributor, you do not pay taxes'
+        
